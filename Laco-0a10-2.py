91f2a3f631fcbe91c29896efbb97eaf4a9a8a1d8
Welbert Hime <welberthime@hotmail.com>
2018-06-11 01:36:17 -0300

    Adicionando arquivos de teste

---
+++ Laco-0a10-2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+if (__name__ == '__main__'):
+	print("Início do laço")
+	for i in range(0,10):
+		print("\t",i)
+	print("Fim do laço")
---
+++ PrintErrado.py
@@ -0,0 +1,4 @@
+#!/bin/usr/env python3
+
+if (__name__ == '__main__'):
+	print("Print com erro"
